799a7eabb3c71ad5376ef744f85019f046763379
Sacit Küheylan <sacit.kuheylan@yandex.com>
2020-10-09 14:26:39 +0300

    Initial Commit

---
+++ CCourses/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  CCourses
+//
+//  Created by Sacit Küheylan on 9.10.2020.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
